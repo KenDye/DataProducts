ef012d78d85f75e5ac23d784350157f516d4396c
KenDye <ken.w.dye@gmail.com>
2016-11-13 16:23:16 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,21 @@
+
+library(shiny)
+
+# Logic required to draw sine wave
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+          
+     
+    x=  seq(0, 2*pi,0.01) 
+    y=(length.out = input$a)*sin((length.out = input$b)*x+(length.out = input$c))
+    plot(x,y,type='l')
+    
+ # Add grid to plot
+    grid(10, col = "lightgray", lty = 1,
+         lwd = par("lwd"), equilogs = TRUE)
+    
+    
+  })
+  
+})
